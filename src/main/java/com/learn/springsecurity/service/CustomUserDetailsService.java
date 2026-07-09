5ac814d1194add2027085b59f5598b5eeae64fcd
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-09 10:44:48 +0330

    Add class CustomUserDetailsService

---
+++ src/main/java/com/learn/springsecurity/service/CustomUserDetailsService.java
@@ -0,0 +1,14 @@
+package com.learn.springsecurity.service;
+
+import com.learn.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+}
