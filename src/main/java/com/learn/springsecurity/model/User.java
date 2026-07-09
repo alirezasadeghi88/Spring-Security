141ca801d36cdfc7940637eca3ccaa53f7378162
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-09 10:22:14 +0330

    Add class User

---
+++ src/main/java/com/learn/springsecurity/model/User.java
@@ -0,0 +1,16 @@
+package com.learn.springsecurity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+}
