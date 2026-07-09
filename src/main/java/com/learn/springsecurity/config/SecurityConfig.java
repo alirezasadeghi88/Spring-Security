6bcca6848c1ca80f02c7e4fc20471c3a5230115d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-09 09:33:46 +0330

    Add class SecurityConfig

--- src/main/java/com/learn/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/learn/springsecurity/config/SecurityConfig.java
@@ -3,8 +3,12 @@ package com.learn.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +18,14 @@ public class SecurityConfig {
     public PasswordEncoder
     passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService
+    userDetailsService() {
+        UserDetails userDetails = User.builder().username("ali")
+                .password(passwordEncoder().encode("1234")).roles("User").build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
