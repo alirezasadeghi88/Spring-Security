645103aa1ef7a316199b3878ba2c1dcf24dcf034
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-09 09:44:35 +0330

    Add class SecurityConfig

--- src/main/java/com/learn/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/learn/springsecurity/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.learn.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -27,5 +30,17 @@ public class SecurityConfig {
                 .password(passwordEncoder().encode("1234")).roles("User").build();
 
         return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+        throws Exception{
+        http.csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 }
