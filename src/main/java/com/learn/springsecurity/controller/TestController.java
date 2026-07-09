4afa9fc7f4a816b1bc2b20ac5d0fdf19d330068d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-09 09:46:29 +0330

    Add class TestController

---
+++ src/main/java/com/learn/springsecurity/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.learn.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/public")
+    public String publicApi() {
+        return "Public API";
+    }
+
+
+}
